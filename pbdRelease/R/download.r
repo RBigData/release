3e2d45af8e9f3b65f0a0badb0b73248bfed4315f
wrathematics <wrathematics@gmail.com>
2018-02-23 14:16:48 -0500

    add downloaders

---
+++ pbdRelease/R/download.r
@@ -0,0 +1,71 @@
+read_spec_file = function(root_path)
+{
+  read.table(paste0(root_path, "/assets/spec.in"), sep=" ", header=TRUE, stringsAsFactors=FALSE)
+}
+
+
+
+check_if_pkg_downloaded = function(dl_path, pkg)
+{
+  length(dir(dl_path, pattern=paste0(pkg, "*"))) == 1
+}
+
+
+
+download_gh = function(dl_path, pkg)
+{
+  need_pkg = !check_if_pkg_downloaded(dl_path, pkg)
+  if (!need_pkg)
+    return(invisible(TRUE))
+  
+  td = tempdir()
+  dir.create(paste0(td, "/pkg"))
+  
+  url = paste0("https://api.github.com/repos/RBigData/", pkg, "/releases/latest")
+  tarball_url = jsonlite::read_json(url)$tarball_url
+  
+  destfile = paste0(td, "/pkg.tar.gz")
+  destdir = paste0(td, "/pkg")
+  curl::curl_download(tarball_url, destfile=destfile)
+  
+  system(paste("tar zxf", destfile, "-C", destdir, "--strip-components 1"))
+  tools::Rcmd(paste("build", destdir))
+  
+  file.remove(destfile)
+  pkg_file = dir(td, pattern="*.tar.gz")
+  file.move(pkg_file, paste0(dl_path, "/", pkg_file))
+  unlink(td, recursive=TRUE)
+  
+  invisible(TRUE)
+}
+
+
+
+download_cran = function(dl_path, pkg)
+{
+  need_pkg = !check_if_pkg_downloaded(dl_path, pkg)
+  
+  if (need_pkg)
+    utils::download.packages(pkg, destdir=dl_path, quiet=TRUE)
+  
+  invisible(TRUE)
+}
+
+
+
+get_packages = function(root_path, release_path)
+{
+  pkgs = read_spec_file(root_path)
+  pkgs.cran = pkgs[which(pkgs$Release.Loc == "cran"), ]$Package
+  pkgs.gh = pkgs[which(pkgs$Release.Loc == "gh"), ]$Package
+  
+  dl_path = paste0(release_path, "/src")
+  
+  for (pkg in pkgs.cran)
+    download_cran(dl_path, pkg)
+  
+  for (pkg in pkgs.gh)
+    download_gh(dl_path, pkg)
+  
+  invisible(TRUE)
+}
