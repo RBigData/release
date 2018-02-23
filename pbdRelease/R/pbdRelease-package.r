6a10f0c6742d35ff0edb2af03205004a47ce7211
wrathematics <wrathematics@gmail.com>
2018-02-23 14:16:53 -0500

    scaffolding

---
+++ pbdRelease/R/pbdRelease-package.r
@@ -0,0 +1,9 @@
+#' pbdRelease
+#' 
+#' Tools for creating pbdR releases. If you aren't part of the pbdR core team,
+#' this probably doesn't interest you.
+#' 
+#' @name pbdRelease-package
+#' @docType package
+#' @keywords Package
+NULL
